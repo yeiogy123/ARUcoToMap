06c0a5b5605cb2941e95f9e8848ab0e50d6b963e
Kerwin <71250456+yeiogy123@users.noreply.github.com>
2024-10-10 15:23:44 +0800

    Update pathplanner.launch.py

--- path_planner_server/launch/pathplanner.launch.py
+++ path_planner_server/launch/pathplanner.launch.py
@@ -3,16 +3,47 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
 
-    controller_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'controller.yaml')
-    default_bt_xml_path = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'behavior.xml')
-    bt_navigator_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'bt_navigator.yaml')
-    planner_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'planner_server.yaml')
-    recovery_yaml = os.path.join(get_package_share_directory('path_planner_server'), 'config', 'recovery.yaml')
+    controller_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'controller.yaml')
+    default_bt_xml_path = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'behavior.xml')
+    planner_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'planner_server.yaml')
+    recovery_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'recovery.yaml')
+    bt_navigator_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'bt_navigator.yaml')
+    nav2_yaml = os.path.join(get_package_share_directory(
+        'localization_server'), 'config', 'amcl_config.yaml')
+    waypoint_follower_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'waypoint_follower.yaml')
+    rviz_config_file_path = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'rviz_config', 'pathplanning.rviz')
+    velocity_smoother_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'velocity_smoother.yaml')
+    smoother_server_yaml = os.path.join(get_package_share_directory(
+        'path_planner_server'), 'config', 'smoother_server.yaml')
+    remappings = [('/tf', 'tf'),
+                  ('/tf_static', 'tf_static')]
+    return LaunchDescription([
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'yaml_filename': '/home/robotic/ros2_ws/Map/9Frtabmap.yaml'}]
+        ),
 
-    
-    return LaunchDescription([     
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            output='screen',
+            parameters=[nav2_yaml]
+        ),
         Node(
             package='nav2_controller',
             executable='controller_server',
@@ -26,13 +57,13 @@ def generate_launch_description():
             name='planner_server',
             output='screen',
             parameters=[planner_yaml]),
-            
+
         Node(
-            package='nav2_recoveries',
-            executable='recoveries_server',
-            name='recoveries_server',
-            parameters=[recovery_yaml],
-            output='screen'),
+            package='nav2_behaviors',
+            executable='behavior_server',
+            name='behavior_server',
+            output='screen',
+            parameters=[recovery_yaml]),
 
         Node(
             package='nav2_bt_navigator',
@@ -42,13 +73,45 @@ def generate_launch_description():
             parameters=[bt_navigator_yaml, {'default_bt_xml_filename': default_bt_xml_path}]),
 
         Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz2_node',
+            arguments=['-d', rviz_config_file_path]),
+        Node(
+            package='nav2_waypoint_follower',
+            executable='waypoint_follower',
+            name='waypoint_follower',
+            output='screen',
+            parameters=[waypoint_follower_yaml]),
+        Node(
+            package='nav2_velocity_smoother',
+            executable='velocity_smoother',
+            name='velocity_smoother',
+            output='screen',
+            parameters=[velocity_smoother_yaml],
+            remappings=remappings +[('cmd_vel', 'cmd_vel_nav'), ('cmd_vel_smoothed', 'cmd_vel')]),
+        Node(
+            package='nav2_smoother',
+            executable='smoother_server',
+            name='smoother_server',
+            output='screen',
+            parameters=[smoother_server_yaml],
+            remappings=remappings),
+
+        Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
-            name='lifecycle_manager_pathplanner',
+            name='lifecycle_manager_navigation',
             output='screen',
             parameters=[{'autostart': True},
-                        {'node_names': ['controller_server',
+                        {'node_names': ['map_server',
+                                        'amcl',
+                                        'controller_server',
                                         'planner_server',
-                                        'recoveries_server',
-                                        'bt_navigator']}])
+                                        'behavior_server',
+                                        'bt_navigator',
+                                        'waypoint_follower',
+                                        'smoother_server',
+                                        'velocity_smoother']}]),
     ])
